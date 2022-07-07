4808ccf5c82407a8cb1ba89944b6a9116eb15001
frantzke <lukas.frantzke@mail.utoronto.ca>
2022-07-07 17:17:20 -0400

    Attempt Github Pages deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
+  // To deploy to github pages
+  router: {
+    base: '/also-known-for/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "Also Known For",
