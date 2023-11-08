0d844534099f57960fb63a198a4022d064168b39
frantzke <lukas.frantzke@mail.utoronto.ca>
2023-11-08 16:10:41 -0500

    Remove error alert

--- pages/index.vue
+++ pages/index.vue
@@ -19,18 +19,8 @@
       </v-btn>
     </div>
 
-    <v-alert
-      :value="true"
-      color="error"
-      icon="$error"
-    >
-      The API Also Known For relies on is down. Work is underway to restore functionality as soon as possible.  
-    </v-alert>
-
     <div v-if="hasError">
-      <h4 class="text-h4 text-center font-weight-light">
-        {{errorMsg}} 😖
-      </h4>
+      <h4 class="text-h4 text-center font-weight-light">{{ errorMsg }} 😖</h4>
     </div>
 
     <div v-if="isLoading">
@@ -59,7 +49,7 @@ export default {
     isLoading: false,
     searchText: "",
     hasError: false,
-    errorMsg: ""
+    errorMsg: "",
   }),
   computed: {
     ...mapGetters(["titles"]),
@@ -72,7 +62,7 @@ export default {
     async onSearch() {
       if (this.searchText === "") {
         this.hasError = true;
-        this.errorMsg = "Please provide a search!"
+        this.errorMsg = "Please provide a search!";
         return;
       }
 
@@ -82,9 +72,9 @@ export default {
         this.resetSearchPage();
         await this.searchTitles({ searchText: this.searchText });
       } catch (err) {
-        this.hasError = true
+        this.hasError = true;
         this.errorMsg = err.message;
-      } finally{
+      } finally {
         this.isLoading = false;
       }
     },
